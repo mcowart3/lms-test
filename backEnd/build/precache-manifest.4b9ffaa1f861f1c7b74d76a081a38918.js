fba92fd7eef5dcd0851a2f121bb6cdcadc0da661
Cowart, Micaiah <mcowart3@gatech.edu>
2021-02-10 14:41:44 -0500

    more tests

---
+++ backEnd/build/precache-manifest.4b9ffaa1f861f1c7b74d76a081a38918.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43c8867af181e134fd3b676e46b2c4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6cd5b4c27b42802355f",
+    "url": "/static/css/main.3246bd3b.chunk.css"
+  },
+  {
+    "revision": "4c3171dacd96467aaafa",
+    "url": "/static/js/2.58e0e746.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.58e0e746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6cd5b4c27b42802355f",
+    "url": "/static/js/main.b433f8bb.chunk.js"
+  },
+  {
+    "revision": "71e0677ab2f28215227f",
+    "url": "/static/js/runtime-main.b234ba30.js"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/static/media/Roboto-Light.d5c46b13.ttf"
+  },
+  {
+    "revision": "14147ca3288fa08b850d029193150722",
+    "url": "/static/media/Roboto-Thin.14147ca3.ttf"
+  },
+  {
+    "revision": "e7967f8efafafaadcf3db1e09cffce45",
+    "url": "/static/media/monbaiti.e7967f8e.ttf"
+  }
+]);
